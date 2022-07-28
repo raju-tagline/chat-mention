742b5b7d92cd6d8813e025d76ebcd6b4f46f9732
raju-tagline <raju.tagline@gmail.com>
2022-07-28 14:27:43 +0530

    Use angular-mentions for chat-mentions

--- src/app/mention/mention.component.ts
+++ src/app/mention/mention.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
+import * as UserData from '../user.json';
 @Component({
   selector: 'app-mention',
   templateUrl: './mention.component.html',
-  styleUrls: ['./mention.component.scss']
+  styleUrls: ['./mention.component.scss'],
 })
 export class MentionComponent implements OnInit {
+  public items: any = Object.values(UserData)[Object.keys(UserData).length - 1];
+  public mentionUserForm!: FormGroup;
+  public selectedUserData: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.mentionUserForm = new FormGroup({
+      user: new FormControl(''),
+    });
   }
 
+  ngOnInit(): void {}
+
+  public submit() {
+    if (this.mentionUserForm.value?.user?.length > 1) {
+      this.items.forEach((res: any) => {
+        const str: string = this.mentionUserForm.value.user.replace(/@/g, '');
+        if (res.name === str) {
+          this.selectedUserData = res;
+        }
+      });
+    } else {
+      this.selectedUserData = {};
+    }
+  }
 }
